9d77a7c2044bfe5640bbc77278434cd54beaab23
Weiming Zhai <wmzhai@gmail.com>
2016-02-03 21:02:14 +0800

    import

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import ContactsList from './ContactsList';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <h1>Contact List</h1>
+        <ContactsList />
       </div>
     );
   }
