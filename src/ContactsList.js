49ff5ba808cfbf650eca50618a58f096d47851d8
Weiming Zhai <wmzhai@gmail.com>
2016-02-03 21:01:50 +0800

    import & export

---
+++ src/ContactsList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ContactsList extends React.Component {
+  render() {
+    return (
+      <ul>
+        <li>Weiming 13910000000</li>
+      </ul>
+    );
+  }
+}
+
+export default ContactsList;
