cbdd385d781ba6c81d6c84f0180b91f19498def1
Weiming Zhai <wmzhai@gmail.com>
2016-02-04 22:42:02 +0800

    mapping data array to view

--- src/app.js
+++ src/app.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ContactsList from './ContactsList';
 
-let contacts = {
-  name: 'Weiming',
+let contacts = [{
+  id: 1,
+  name: 'Thomas',
   phone: '13110000000'
-};
+},{
+  id: 2,
+  name: 'Tom',
+  phone: '13210000000'
+},{
+  id: 3,
+  name: 'Jerry',
+  phone: '13310000000'
+},{
+  id: 4,
+  name: 'Hanks',
+  phone: '13410000000'
+}];
 
 class App extends React.Component {
   render() {
